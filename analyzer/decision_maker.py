376ab35a27930eb9ad54dcbec59e433f08900770
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2022-08-26 05:36:09 +0700

    Update decision_maker.py

--- analyzer/decision_maker.py
+++ analyzer/decision_maker.py
@@ -1,1 +1,28 @@
+import cv2 as cv
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+
+class DecisionMaker:
+    def __init__(self):
+        self.out = tf.keras.models.load_model('../models/Viktor_vgg16')
+        # self.__weights = tf.keras.models.load_model('my_model_weights.h5')
+
+    def predict(self, img) -> bool:
+        # res_img = img.swapaxes(1, 0)
+        img = cv.resize(img, (200, 200))
+        conv_img = Image.fromarray(img, 'RGB')
+        img_array = np.array(conv_img)
+        img_array = np.expand_dims(img_array, axis=0)
+        print(img.shape, img_array.shape)
+        predicted_val = self.out.predict(img_array)[0][0]
+        return predicted_val
+
+
+if __name__ == '__main__':
+  AI = DecisionMaker()
+  img = cv.imread('F:/Serge/Downloads/Telegram Desktop/dataset/dataset/train/nearby/img.94.png')
+  pr = AI.predict(img)
+  print(pr)
 
